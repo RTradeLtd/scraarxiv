4a6209be1c862bc07fa319f73afbc592f971931f
potsables <postables@rtradetechnologies.com>
2018-11-09 17:45:36 -0800

    scraarxiv: added cpmmand

--- cmd/scraarxiv/scraarxiv.go
+++ cmd/scraarxiv/scraarxiv.go
@@ -3,6 +3,10 @@ package scraarxiv
 import (
 	"log"
 	"os"
+	"strconv"
+
+	"github.com/RTradeLtd/scraarxiv/magnifier"
+	"github.com/RTradeLtd/scraarxiv/searcher"
 
 	"github.com/RTradeLtd/cmd"
 	"github.com/RTradeLtd/config"
@@ -20,7 +24,22 @@ var commands = map[string]cmd.Cmd{
 		Blurb:       "Starts our arxiv scraper",
 		Description: "Scrapes arxiv and takes papers from arxiv, pins it through temporal, and scrapes it",
 		Action: func(cfg config.TemporalConfig, args map[string]string) {
-
+			pageCountInt, err := strconv.ParseInt(args["pageCount"], 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			glass, err := magnifier.NewGlassClient(&cfg)
+			if err != nil {
+				log.Fatal(err)
+			}
+			urls, err := searcher.Search(args["searchTerms"], pageCountInt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			pdfURLs := searcher.ExtractPDFURLs(urls)
+			if err = glass.Magnify(pdfURLs); err != nil {
+				log.Fatal(err)
+			}
 		},
 	},
 }
@@ -45,8 +64,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	searchTerms := os.Getenv("SEARCH_TERMS")
+	if searchTerms == "" {
+		log.Fatal("SEARCH_TERMS env var is empty")
+	}
+	pageCount := os.Getenv("PAGE_COUNT")
+	if pageCount == "" {
+		pageCount = "1"
+	}
 	// load arguments
-	flags := make(map[string]string)
+	flags := map[string]string{
+		"searchTerms": searchTerms,
+		"pageCount":   pageCount,
+	}
 
 	// run our program
 	os.Exit(temporal.Run(*tCfg, flags, os.Args[1:]))
